0a18e9b223b4d1abd8bdea4e72bfcd86cee3e9bd
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2021-02-20 12:47:40 +0100

    resolve date time individual

--- src/main/java/pl/sda/zdjavapol73/tasks/individual/DateTimeIndividual.java
+++ src/main/java/pl/sda/zdjavapol73/tasks/individual/DateTimeIndividual.java
@@ -2,6 +2,8 @@ package pl.sda.zdjavapol73.tasks.individual;
 
 import pl.sda.zdjavapol73.api.Task;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class DateTimeIndividual implements Task {
@@ -12,15 +14,17 @@ public class DateTimeIndividual implements Task {
     @Override
     public void run() {
         // create ZonedDateTime 15.12.2015 21:47 in zone Asia/Aden
-        ZonedDateTime timeInAsiaAden = ZonedDateTime.now();
-
+        final ZonedDateTime timeInAsiaAden =
+                ZonedDateTime.of(LocalDateTime.of(2015, 12, 15, 21, 47), ZoneId.of("Asia/Aden"));
+        System.out.println("Time in Asia/Aden:  " + timeInAsiaAden);
         // convert this time to UTC
-        ZonedDateTime timeInUtc = ZonedDateTime.now();
-
+        final ZonedDateTime timeInUtc = timeInAsiaAden.withZoneSameInstant(ZoneId.of("UTC"));
+        System.out.println("Time in Asia/Aden in convertion to UTC:  " + timeInUtc);
         // UTC convert to zone America/Noronha
-        ZonedDateTime timeInAmericaNoronha = ZonedDateTime.now();
-
-        if (timeInAsiaAden.isEqual(timeInUtc) && timeInAmericaNoronha.isEqual(timeInUtc)) {
+        final ZonedDateTime timeInAmericaNoronha = timeInUtc.withZoneSameInstant(ZoneId.of("America/Noronha"));
+        System.out.println("Time from UTC to America/Noronha:  " + timeInAmericaNoronha);
+        if (timeInAsiaAden.isEqual(timeInUtc) && timeInAmericaNoronha.isEqual(timeInUtc) &&
+                timeInAmericaNoronha.isEqual(timeInAsiaAden)) {
             System.out.println("Success!!!");
         } else {
             System.out.println("Try again...");
