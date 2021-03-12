004f8438e5ba110c9f5aacb9a88a1f531c104df2
Karol <karol@jubilex.com.pl>
2021-03-12 20:36:44 +0100

    Finished task 3 from Enum Tasks list

---
+++ src/main/java/pl/sda/zdjavapol73/homework/enumTasks/task3/WrongInputData.java
@@ -0,0 +1,7 @@
+package pl.sda.zdjavapol73.homework.enumTasks.task3;
+
+public class WrongInputData extends Exception{
+    public WrongInputData(){
+        super("WRONG INPUT DATA, PLEASE TRY AGAIN");
+    }
+}
